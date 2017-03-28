891ed1704ddfce6d53dbfd35f8e8ea7835d6e19e
grant <gsbackes@gmail.com>
2017-03-28 19:20:11 +0900

    finish simple cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,5 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-    @set_time = Time.now.strftime('%I:%M %p')
-    @set_date = Time.now.strftime('%v')
   end
 end
